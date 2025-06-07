712c2e696c0e92368507c298798f568f9a59ad2a
jeet <jeetrathodinitiontechmedia@gmail.com>
2025-06-07 10:34:05 +0530

    update order

--- dist/utils/commonFunction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolveStatus = void 0;
-const resolveStatus = (status) => {
-    return status === null || status === undefined ? true : status;
-};
-exports.resolveStatus = resolveStatus;
-//# sourceMappingURL=commonFunction.js.map
